a301051785035eec2c63c6dfd366f0a247039cb5
sample <tobabu@gmail.com>
2017-04-18 12:56:36 +0100

    Added test specs for multi buy offer for Apples and Oranges

--- test/shopping/cart/application/test/ApplicationSpec.scala
+++ test/shopping/cart/application/test/ApplicationSpec.scala
@@ -31,16 +31,16 @@ class ApplicationSpec extends PlaySpec with OneAppPerTest {
       route(app, FakeRequest(GET, "/checkout/orange")).map(contentAsString(_)) mustBe Some("0.25")
     }
     
-    "checkout must price for multi apple" in {
-      route(app, FakeRequest(GET, "/checkout/apple,apple,apple")).map(contentAsString(_)) mustBe Some("1.80")
+    "checkout must total for 2 apples for the price of 1" in {
+      route(app, FakeRequest(GET, "/checkout/apple,apple,apple")).map(contentAsString(_)) mustBe Some("1.20")
     }
     
-    "checkout must price for multi oranges" in {
-      route(app, FakeRequest(GET, "/checkout/orange,orange")).map(contentAsString(_)) mustBe Some("0.50")
+    "checkout must total for 3 oranges for the price of 2" in {
+      route(app, FakeRequest(GET, "/checkout/orange,orange,orange")).map(contentAsString(_)) mustBe Some("0.50")
     }
     
-    "checkout must price for multi apple and onrages" in {
-      route(app, FakeRequest(GET, "/checkout/orange,orange,apple,apple")).map(contentAsString(_)) mustBe Some("1.70")
+    "checkout must total for multi apple and onrages" in {
+      route(app, FakeRequest(GET, "/checkout/orange,orange,apple,apple")).map(contentAsString(_)) mustBe Some("1.10")
     }
 
   }
